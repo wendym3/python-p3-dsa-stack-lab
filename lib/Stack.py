19b38077caf3dc2dd0772e7539275a5255703bfa
wendymasese <wendy.masese@student.moringaschool.com>
2024-07-24 16:43:42 +0300

    complete lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,41 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
+    
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
+      
 
-    def push(self, item):
-        pass
+    def push(self, items):
+       if len(self.items) < self.limit:
+         self.items.append(items)
+    
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+     
 
     def peek(self):
-        pass
+        if not self.isEmpty:
+            return self.items[-1]
+        
+       
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) >= self.limit
+        
 
     def search(self, target):
-        pass
+        try:
+            index =self.items.index(target)
+            return len(self.items) - index -1
+        except ValueError:
+            return -1
+        
